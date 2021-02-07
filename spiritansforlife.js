c7046d2142581b6c984b2af6502bcf512fe4c003
Nitodevs <74937472+Nitodevs@users.noreply.github.com>
2021-02-07 01:34:26 +0100

    Add files via upload

---
+++ spiritansforlife.js
@@ -0,0 +1,8 @@
+function openNav(){
+  document.getElementById("sidebar").style.width="225px";
+  document.getElementById("main").style.marginLeft="0px";
+}
+function closeNav(){
+  document.getElementById("sidebar").style.width="0px";
+  document.getElementById("main").style.marginLeft="0px";
+}
