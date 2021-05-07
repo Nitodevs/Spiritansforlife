457a909540d1d4c736d4325275c73566bacf011f
Nitodevs <74937472+Nitodevs@users.noreply.github.com>
2021-05-07 21:55:16 +0100

    Add files via upload

---
+++ spiritansforlifeadmin.js
@@ -0,0 +1,24 @@
+function openNav(){
+  document.getElementById("sidebar").style.width="225px";
+  document.getElementById("main").style.marginLeft="0px";
+}
+function closeNav(){
+  document.getElementById("sidebar").style.width="0px";
+  document.getElementById("main").style.marginLeft="0px";
+}
+
+
+ClassicEditor
+    .create( document.querySelector( '#body' ), {
+        toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+        heading: {
+            options: [
+                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+            ]
+        }
+    } )
+    .catch( error => {
+        console.log( error );
+    } );
